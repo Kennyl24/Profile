29ca7d112c0d958bf8ddb26912b6badb8820f3b2
Kenneth <kenny.laprelle@gmail.com>
2018-07-24 08:02:18 -0700

    nav menu changes

--- client/src/containers/NavMenuFooter.jsx
+++ client/src/containers/NavMenuFooter.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 
 import IconButton from '@material-ui/core/IconButton';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: 'rgb(63, 6, 245)' },
+    secondary: { main: 'rgb(63, 6, 245)' }
+  },
+});
 
 
 class NavMenuFooter extends React.Component {
@@ -20,11 +26,13 @@ class NavMenuFooter extends React.Component {
 
   render() {
     return (
+      <MuiThemeProvider theme={theme}>
       <div style={{position:'absolute', bottom: '10px', fontSize:'14px', color:'white'}}>
-      <IconButton color="secondary"  aria-label="Add an alarm">
-        <Icon>linkedin</Icon>
+      <IconButton color="secondary" aria-label="previous song">
+        <Icon>skip_previous</Icon>
       </IconButton>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
