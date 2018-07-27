25dc52ee890b6080779dfeab26bedee72eb324a3
Kenneth <kenny.laprelle@gmail.com>
2018-07-26 17:16:07 -0700

    deploy env changes

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/contact');
+mongoose.connect('process.env.MONGODB_URI');
 const db = mongoose.connection;
 
 db.on('error', function(err) {
--- server/index.js
+++ server/index.js
@@ -37,6 +37,6 @@ app.get('/Resume', (request, response) => {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
